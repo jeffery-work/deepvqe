c80c07344568d94c56223c0e1ca37b3f97961229
jeffery <jeffery_work@163.com>
2024-03-12 23:46:29 +0800

    first commit

---
+++ refactor/deepvqe_module.py
@@ -0,0 +1,12 @@
+"""
+# File       : deepvqe_module.py
+# Time       : 2024/3/12 23:39
+# Author     : fei jie
+# version    : 1.0
+# Description: 
+"""
+
+import torch.nn as nn
+
+class StreamConv1d(nn.module):
+    def __init__(self):
